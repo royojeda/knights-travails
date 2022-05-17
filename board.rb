1a241bc10b1556c39fd33db5b34e840623e88eed
Dennis Ojeda <royojeda1@gmail.com>
2022-05-17 22:50:38 +0800

    Update the #build_board method
    Now creates a Move object for every spot in the 8x8 board. Each Move is
    connected other Moves if the knight can move from one to the other in a
    single turn.

--- board.rb
+++ board.rb
@@ -1,13 +1,31 @@
 require_relative 'move'
 
 class Board
-  attr_reader :root
+  attr_accessor :root
 
   def initialize
     @root = build_board
   end
 
-  def build_board(location = [0, 0])
-    Move.new(location)
+  def build_board(x = 0, y = 0, made = [])
+    case [x, y]
+    in [0..7, 0..7] unless made.include?([x, y])
+      current = Move.new(x, y)
+      made.push([x, y])
+
+      current.wnw = build_board(x - 2, y + 1, made)
+      current.nnw = build_board(x - 1, y + 2, made)
+      current.nne = build_board(x + 1, y + 2, made)
+      current.ene = build_board(x + 2, y + 1, made)
+      current.ese = build_board(x + 2, y - 1, made)
+      current.sse = build_board(x + 1, y - 2, made)
+      current.ssw = build_board(x - 1, y - 2, made)
+      current.wsw = build_board(x - 2, y - 1, made)
+    else
+      return
+    end
+    puts "#{made}\n\n"
+    puts "#{made.length}\n\n"
+    current
   end
 end
--- move.rb
+++ move.rb
@@ -1,68 +1,16 @@
 class Move
   attr_reader :location
+  attr_writer :wnw, :nnw, :nne, :ene, :ese, :sse, :ssw, :wsw
 
-  def initialize(location)
-    @location = location
-    add_children
-  end
-
-  def add_children
-    @wnw = calc_wnw
-    @nnw = calc_nnw
-    @nne = calc_nne
-    @ene = calc_ene
-    @ese = calc_ese
-    @sse = calc_sse
-    @ssw = calc_ssw
-    @wsw = calc_wsw
-  end
-
-  def calc_wnw
-    child = [location[0] - 2, location[1] + 1]
-    valid?(child)
-  end
-
-  def calc_nnw
-    child = [location[0] - 1, location[1] + 2]
-    valid?(child)
-  end
-
-  def calc_nne
-    child = [location[0] + 1, location[1] + 2]
-    valid?(child)
-  end
-
-  def calc_ene
-    child = [location[0] + 2, location[1] + 1]
-    valid?(child)
-  end
-
-  def calc_ese
-    child = [location[0] + 2, location[1] - 1]
-    valid?(child)
-  end
-
-  def calc_sse
-    child = [location[0] + 1, location[1] - 2]
-    valid?(child)
-  end
-
-  def calc_ssw
-    child = [location[0] - 1, location[1] - 2]
-    valid?(child)
-  end
-
-  def calc_wsw
-    child = [location[0] - 2, location[1] - 1]
-    valid?(child)
-  end
-
-  def valid?(child)
-    case child
-    in [0..8, 0..8]
-      child
-    else
-      nil
-    end
+  def initialize(x, y)
+    @location = [x, y]
+    @wnw = nil
+    @nnw = nil
+    @nne = nil
+    @ene = nil
+    @ese = nil
+    @sse = nil
+    @ssw = nil
+    @wsw = nil
   end
 end
